372ae937506c338ace2700c94fa4a8e8ca6b3510
Franklyn Seabra Rogerio Bezerra <franklyn.bezerra@serpro.gov.br>
2025-03-29 10:05:19 -0300

    Adiciona auto scale e logs

--- pedidos/src/test/java/br/com/alurafood/pedidos/PedidosApplicationTests.java
+++ pedidos/src/test/java/br/com/alurafood/pedidos/PedidosApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.alurafood.pedidos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 
 @SpringBootTest
+@TestConfiguration
 class PedidosApplicationTests {
 
 	@Test
